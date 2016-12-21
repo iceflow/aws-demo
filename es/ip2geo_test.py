e478c7b9a8a9ede32fb1679abdc7d2957a8c0a97
Leo Chen <lintchen@amazon.com>
2016-12-21 15:29:15 +0000

    def get_geo_location
    Signed-off-by: Leo Chen <lintchen@amazon.com>

--- es/ip2geo_test.py
+++ es/ip2geo_test.py
@@ -13,7 +13,12 @@ response = client.insights('128.101.101.101')
 
 import geoip2.database
 import pprint
-reader = geoip2.database.Reader('/data/aws-demo/es/GeoLite2-City.mmdb')
-response = reader.city('128.101.101.101')
 
-print response
+def get_geo_location(host_ip):
+    reader = geoip2.database.Reader('/data/aws-demo/es/GeoLite2-City.mmdb')
+    response = reader.city(host_ip)
+
+    return ("{lat}, {lon}".format(lat=response.location.latitude, lon=response.location.longitude))
+
+
+print get_geo_location('128.101.101.101')
