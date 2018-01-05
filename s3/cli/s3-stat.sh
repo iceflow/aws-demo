59e354c037546bfeb4827889035e55d874c44b38
Leo Chen <chenlt78@qq.com>
2018-01-05 10:22:42 +0800

    Adding s3 bucket location information
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- s3/cli/s3-stat.sh
+++ s3/cli/s3-stat.sh
@@ -16,12 +16,14 @@ get_s3_bucket_stat()
 
 #--query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId]
 
+	Location=$(aws --profile $PROFILE s3api get-bucket-location --bucket ${BUCKET_NAME} --output text)
+
 	#aws --profile $PROFILE cloudwatch get-metric-statistics --metric-name BucketSizeBytes --namespace AWS/S3 --start-time ${TODAY}T00:00:00Z --end-time ${TOMORROW}T00:00:00Z --statistics Average --unit Bytes --region $REGION --dimensions Name=BucketName,Value=${BUCKET_NAME} Name=StorageType,Value=StandardStorage --period 86400 --output json --query '[Label, Datapoints.[Average, Timestamp, Unit]]'
 	BucketSizeBytes=$(aws --profile $PROFILE cloudwatch get-metric-statistics --metric-name BucketSizeBytes --namespace AWS/S3 --start-time ${TODAY}T00:00:00Z --end-time ${TOMORROW}T00:00:00Z --statistics Average --unit Bytes --region $REGION --dimensions Name=BucketName,Value=${BUCKET_NAME} Name=StorageType,Value=StandardStorage --period 86400 --output text --query '[Datapoints[0].[Average,Timestamp,Unit]]')
 
 	NumberOfObjects=$(aws --profile $PROFILE cloudwatch get-metric-statistics --metric-name NumberOfObjects --namespace AWS/S3 --start-time ${TODAY}T00:00:00Z --end-time ${TOMORROW}T00:00:00Z --statistics Average --unit Count --region $REGION --dimensions Name=BucketName,Value=${BUCKET_NAME} Name=StorageType,Value=AllStorageTypes --period 86400 --output text --query '[Datapoints[0].[Average,Timestamp,Unit]]')
 
-	echo "${BUCKET_NAME} BucketSizeBytes ${BucketSizeBytes} NumberOfObjects ${NumberOfObjects}"
+	echo "${BUCKET_NAME} ${Location} BucketSizeBytes ${BucketSizeBytes} NumberOfObjects ${NumberOfObjects}"
 
 }
 
