465c44b65e9fe4ae8779102d9ae61003be622275
Leo Chen <lintchen@amazon.com>
2018-01-15 20:29:49 +0800

    Adding check_client_request.php to monitor HTTP request client/http header/instance related information
    Signed-off-by: Leo Chen <lintchen@amazon.com>

---
+++ web/sample_pages/check_client_request.php
@@ -0,0 +1,22 @@
+<?php
+  $url = "http://169.254.169.254/latest/meta-data/instance-id";
+  $instance_id = file_get_contents($url);
+  echo "Instance ID: <b>" .$instance_id ."</b><br/>";
+  $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
+  $zone = file_get_contents($url);
+  echo "Zone: <b>" .$zone ."</b><br/>";
+
+
+  $iipp = $_SERVER["REMOTE_ADDR"];
+  echo "Client IP:".$iipp."<br/>";
+
+  echo "<br/><b> Request HTTP header list </b><br/>";
+    
+  $headers = apache_request_headers();
+
+  foreach ($headers as $header => $value) {
+    echo "$header: $value <br />\n";
+  }
+
+  //phpinfo();
+?>
