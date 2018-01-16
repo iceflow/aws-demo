3c7f0e0f685ea23d26644ac49ab6434d83d96206
Leo Chen <chenlt78@qq.com>
2018-01-16 17:43:29 +0800

    Adding displaying request time
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- web/sample_pages/check_client_request.php
+++ web/sample_pages/check_client_request.php
@@ -1,7 +1,10 @@
 <?php
   $url = "http://169.254.169.254/latest/meta-data/instance-id";
   $instance_id = file_get_contents($url);
-  echo "Instance ID: <b>" .$instance_id ."</b><br/>";
+
+  print_r(date('Y/m/d H:i:s'));
+
+  echo "<br/><br/>Instance ID: <b>" .$instance_id ."</b><br/>";
   $url = "http://169.254.169.254/latest/meta-data/placement/availability-zone";
   $zone = file_get_contents($url);
   echo "Zone: <b>" .$zone ."</b><br/>";
@@ -11,7 +14,7 @@
   echo "Client IP:".$iipp."<br/>";
 
   echo "<br/><b> Request HTTP header list </b><br/>";
-    
+
   $headers = apache_request_headers();
 
   foreach ($headers as $header => $value) {
