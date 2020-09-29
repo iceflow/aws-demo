a0d71a302b2f80fe936184bac7afd35870ad935f
Leo Chen <chenlt78@qq.com>
2020-09-30 00:15:02 +0800

    Delete S3 bucket recursively
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- s3/cli/s3_force_delete.sh
+++ s3/cli/s3_force_delete.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 PROFILE=${1:-default}
-B=$2
+BUCKETS=$2
 DEBUG=false
 
-echo -n "Delete objects in $B..."
-aws --profile $PROFILE s3 rm --recursive s3://$B > /dev/null 2>&1
-echo -n "Done"
-echo "Beging to delete bucket ..."
-aws --profile $PROFILE s3 rb s3://$B
-echo "done"
+for B in $BUCKETS; do
+	echo -n "Delete objects in $B..."
+	aws --profile $PROFILE s3 rm --recursive s3://$B > /dev/null 2>&1
+	echo "Done"
+	echo "Beging to delete bucket ..."
+	aws --profile $PROFILE s3 rb s3://$B
+	echo "done"
+done
